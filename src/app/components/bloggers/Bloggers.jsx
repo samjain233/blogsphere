0083c3598a8dab42791bcc2478efb6cc549c7ca5
Prince Kumar <Cprince4097@gmail.com>
2024-03-10 13:20:43 +0530

    created bloggers card

--- src/app/components/bloggers/Bloggers.jsx
+++ src/app/components/bloggers/Bloggers.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+const categoriesList=["user1","user2","user3","user4","user5","user6","user7","user8"]
+
 const Bloggers = () => {
   return (
-    <div>Bloggers</div>
+    <>
+      <div className='flex flex-wrap justify-center items-center sm:px-[8%] lg:px-[10%] 2xl:px-[12%] px-4'>
+    {categoriesList.map((element,index)=>{
+    return <div key={index} className='rounded-circle'>
+        <h2 className=' font-bold text-xl sm:text-2xl border border-black px-8 py-4 rounded-full bg-lime-200 m-2 ' style={{ width: '150px', height: '150px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>{element}</h2>
+    </div>
+})}
+    </div>
+    </>
   )
 }
 
