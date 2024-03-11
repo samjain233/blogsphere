303a95a4718594ab8204ee7284cba50b0e283952
Kiran <kiran2022003kumari@gmail.com>
2024-03-11 22:32:30 +0530

    getblog added

--- backend/routes/Blog/blog.routes.js
+++ backend/routes/Blog/blog.routes.js
@@ -1,8 +1,8 @@
 import { createBlogController } from '../../controllers/blog/createblog.controller.js';
-
+import { getBlogController } from './../../controllers/blog/getblog.controller.js';
 import express from "express";
 const router = express.Router();
-
-router.post("/postblog",createBlogController );
+router.get("/postblog",createBlogController );
+router.post("/getblog",getBlogController );
 
 export default router;
