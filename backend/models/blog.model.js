c964de6c7614db63653982c92805064c31d70326
samjain233 <94921996+samjain233@users.noreply.github.com>
2024-03-14 21:56:53 +0530

    mereged from main

--- backend/models/blog.model.js
+++ backend/models/blog.model.js
@@ -1,61 +1,65 @@
 import mongoose from "mongoose";
 
-const blogSchema = new mongoose.Schema({
-    slug: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    author:{
-        type: mongoose.Schema.Types.ObjectId, ref: "User",
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    keywords: {
-        type: Array,
-        required: true,
-        default: [],
-    },
-    content: {
-        type: String,
-        required: true,
-    },
-    imageUrl: {
-        type: String,
-    },
-    category:{
-        type:String,
-    },
-    active: {
-        type: Boolean,
-        default: true,
-    },
-    comments: [
-        {
-            name: String,
-            comment: String,
+const blogSchema = new mongoose.Schema(
+    {
+        slug: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        keywords: {
+            type: Array,
+            required: true,
             default: [],
         },
-    ],
-    upvotes: [
-        { type: mongoose.Schema.Types.ObjectId, ref: "User", default: [] },
-    ],
-    views: {
-        type: Number,
-        default: 0,
-    },
-    monthlyViews: [
-        {
-            month: String,
-            views: Number,
-            default: [],
+        content: {
+            type: String,
+            required: true,
         },
-    ],
-},{
-    timestamps: true,
-});
+        imageUrl: {
+            type: String,
+        },
+        category: {
+            type: String,
+        },
+        active: {
+            type: Boolean,
+            default: true,
+        },
+        comments: [
+            {
+                name: String,
+                comment: String,
+                default: [],
+            },
+        ],
+        upvotes: [
+            { type: mongoose.Schema.Types.ObjectId, ref: "User", default: [] },
+        ],
+        views: {
+            type: Number,
+            default: 0,
+        },
+        monthlyViews: [
+            {
+                month: String,
+                views: Number,
+                default: [],
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    },
+);
 
 const Blog = mongoose.model("Blog", blogSchema);
 export default Blog;
