7336da80d9c3202d57aa31f70f83cdbb7100b1e8
Kiran <kiran2022003kumari@gmail.com>
2024-03-14 08:16:29 +0530

    error in updateactiveblog api

--- backend/routes/Blog/blog.routes.js
+++ backend/routes/Blog/blog.routes.js
@@ -1,11 +1,13 @@
 import { createBlogController } from '../../controllers/blog/createblog.controller.js';
 import { getallBlogsController } from '../../controllers/blog/getallblogs.controller.js';
+import { updateactiveBlogController } from '../../controllers/blog/updateactiveblog.controller.js';
 import { getBlogController } from './../../controllers/blog/getblog.controller.js';
 import express from "express";
 const router = express.Router();
 router.get("/postblog",createBlogController );
 router.post("/getblog",getBlogController );
 router.get("/getallblogs", getallBlogsController);
+router.put("/updateactiveblog", updateactiveBlogController);
 
 
 export default router;
