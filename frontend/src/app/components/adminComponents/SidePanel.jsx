2dc89612acf75d7b0cdfa94f0a52cd5761f8c64f
Prince Kumar <Cprince4097@gmail.com>
2024-03-14 22:21:28 +0530

    fixed home button

--- frontend/src/app/components/adminComponents/SidePanel.jsx
+++ frontend/src/app/components/adminComponents/SidePanel.jsx
@@ -1,7 +1,11 @@
+import { useRouter } from "next/navigation";
 import React from "react";
 const list = ["Dashboard", "Create Post", "Manage Post", "Home", "Logout"];
 
 const SidePanel = ({tab , setTab}) => {
+
+const router=useRouter()
+
   return (
     <>
       <div className="bg-gray-200 p-2 h-screen shadow-md sticky top-0">
@@ -10,7 +14,7 @@ const SidePanel = ({tab , setTab}) => {
             <div
               key={index}
               className={`${tab===element?"bg-gray-500 text-white":"bg-gray-300"} p-4 mt-2 cursor-pointer hover:bg-gray-400 transition-all duration-200`}
-              onClick={()=>setTab(element)}
+              onClick={()=>{if(element==="Home"){router.push("/")};setTab(element)}}
             >
               <p>{element}</p>
             </div>
